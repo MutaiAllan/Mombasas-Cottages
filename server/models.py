d18d7ff74fa31b7db797e2b8a1692fa2ad0cfd41
Joy Olago <joydanielle62@gmail.com>
2023-10-02 12:23:25 +0300

    created class User and defined its relationships

--- server/models.py
+++ server/models.py
@@ -3,3 +3,13 @@ from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.orm import validate
 
 db = SQLAlchemy()
+
+class User(db.Model):
+    
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    
+    # Define one-to-many relationship with reviews
+    reviews = db.relationship('Review', backref='user', lazy=True)
