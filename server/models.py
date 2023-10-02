092cded53a311263243b950bd7956f066a272c47
Joy Olago <joydanielle62@gmail.com>
2023-10-02 12:34:23 +0300

    form validation code

--- server/models.py
+++ server/models.py
@@ -1,6 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.orm import validate
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, TextAreaField, validators
 
 db = SQLAlchemy()
 
@@ -36,3 +38,20 @@ class Review(db.Model):
     
     # Define many-to-one relationship with dog houses
     dog_house_id = db.Column(db.Integer, db.ForeignKey('dog_house.id'), nullable=False)
+
+class UserForm(FlaskForm):
+    username = StringField('Username', [validators.Length(min=4, max=80), validators.DataRequired()])
+    email = StringField('Email', [validators.Email(), validators.DataRequired()])
+    password = StringField('Password', [validators.Length(min=6), validators.DataRequired()])
+
+class DogHouseForm(FlaskForm):
+    name = StringField('Name', [validators.Length(min=2, max=100), validators.DataRequired()])
+    location = StringField('Location', [validators.Length(min=1, max=200), validators.DataRequired()])
+    description = TextAreaField('Description', [validators.Length(max=200)])
+
+class ReviewForm(FlaskForm):
+    rating = IntegerField('Rating', [validators.NumberRange(min=1, max=5), validators.DataRequired()])
+    content = TextAreaField('Content', [validators.Length(max=1000)])
+
+
+
