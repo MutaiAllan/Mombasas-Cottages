94b361259cdb05a8b89c3640477950ef9d20f631
WinterJackson <winterjacksonwj@gmail.com>
2023-10-03 11:08:41 +0300

    corrected app.py

--- server/app.py
+++ server/app.py
@@ -7,7 +7,6 @@ from models import db, DogHouse, DogHouseForm
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://dog_house_db_user:IpxSKYa5XsKJnHSqlrAnXPAvBeHPN0L4@dpg-ckdh6tkiibqc73cm9ot0-a.oregon-postgres.render.com/dog_house_db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# app.jsonify_prettify = False  # Adjusted from app.json.compact
 
 # Initialize the SQLAlchemy instance
 app.json.compact = False
@@ -17,14 +16,14 @@ db.init_app(app)
 
 api = Api(app)
 
-# Example route to get a list of dog houses
-@app.route('/api/dog_houses', methods=['GET'])
+# Route to get a list of dog houses
+@app.route('/dog_houses', methods=['GET'])
 def get_dog_houses():
     dog_houses = DogHouse.query.all()
     dog_houses_data = [dog_house.serialize() for dog_house in dog_houses]
     return jsonify(dog_houses_data)
 
-# Example route to create a new dog house
+# Route to create a new dog house
 @app.route('/dog_houses', methods=['POST'])
 def create_dog_house():
     form = DogHouseForm(request.form)
@@ -36,7 +35,7 @@ def create_dog_house():
     else:
         return jsonify({'error': 'Invalid data'})
 
-# Example route to get a specific dog house by ID
+# Route to get a specific dog house by ID
 @app.route('/dog_houses/<int:dog_house_id>', methods=['GET'])
 def get_dog_house(dog_house_id):
     dog_house = DogHouse.query.get(dog_house_id)
@@ -45,7 +44,7 @@ def get_dog_house(dog_house_id):
     else:
         return jsonify({'error': 'Dog house not found'})
 
-# Add routes for other CRUD actions (update and delete) and for other models (User and Review)
+# Add routes 
 
 if __name__ == '__main__':
     app.run(debug=True)
