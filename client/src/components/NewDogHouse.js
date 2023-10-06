64e5cac17f9599916c0f4740ce52cb8e94271ac1
Joy Olago <joydanielle62@gmail.com>
2023-10-06 11:05:54 +0300

    styled form for adding new dog house

--- client/src/components/NewDogHouse.js
+++ client/src/components/NewDogHouse.js
@@ -31,49 +31,71 @@ function NewDogHouse() {
 
 
     return (
-        <div>
-          <form onSubmit={handleSubmit}>
-            <h1>Add new dog house</h1>
-            <label htmlFor="name">name</label>
-            <input
-              type="text"
-              id="name"
-              autoComplete="off"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            /><br></br>
-
-            <label htmlFor="location">Location</label>
-            <input
-              type="text"
-              id="location"
-              autoComplete="off"
-              value={location}
-              onChange={(e) => setLocation(e.target.value)}
-            /><br></br>
-
-            <label htmlFor="description">Description</label>
-            <input
-              type="text"
-              id="description"
-              value={description}
-              onChange={(e) => setDescription(e.target.value)}
-              autoComplete="off"
-            /><br></br>
-
-            <label htmlFor="image">Image</label>
-            <input
-              type="text"
-              id="image"
-              value={image}
-              onChange={(e) => setImage(e.target.value)}
-              autoComplete="off"
-            /><br></br>
-            <button type="submit">Submit</button>
-          </form>
-
+      <div className="container mt-5">
+      <form onSubmit={handleSubmit}>
+        <h1>Add new dog house</h1>
+        <div className="mb-3">
+          <label htmlFor="name" className="form-label">
+            Name
+          </label>
+          <input
+            type="text"
+            id="name"
+            autoComplete="off"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            className="form-control"
+          />
         </div>
-      );
+
+        <div className="mb-3">
+          <label htmlFor="location" className="form-label">
+            Location
+          </label>
+          <input
+            type="text"
+            id="location"
+            autoComplete="off"
+            value={location}
+            onChange={(e) => setLocation(e.target.value)}
+            className="form-control"
+          />
+        </div>
+
+        <div className="mb-3">
+          <label htmlFor="description" className="form-label">
+            Description
+          </label>
+          <input
+            type="text"
+            id="description"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            autoComplete="off"
+            className="form-control"
+          />
+        </div>
+
+        <div className="mb-3">
+          <label htmlFor="image" className="form-label">
+            Image
+          </label>
+          <input
+            type="text"
+            id="image"
+            value={image}
+            onChange={(e) => setImage(e.target.value)}
+            autoComplete="off"
+            className="form-control"
+          />
+        </div>
+
+        <button type="submit" className="btn btn-info">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default NewDogHouse;
