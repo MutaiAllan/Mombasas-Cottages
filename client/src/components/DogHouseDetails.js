d23fa642d1df99aa7e94429277a4c3b67fcb63e2
whiplade <nmawira13@gmail.com>
2023-10-09 21:33:57 +0300

    removed spinner

--- client/src/components/DogHouseDetails.js
+++ client/src/components/DogHouseDetails.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { Spinner } from "react-bootstrap"
 
 function DogHouseDetails() {
     const { id } = useParams();
