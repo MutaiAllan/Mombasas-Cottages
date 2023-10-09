dfd7fcb35bb3bac87aa4930cd878af3409f57e7f
WinterJackson <winterjacksonwj@gmail.com>
2023-10-09 15:21:09 +0300

    corrected app.py

--- server/config.py
+++ server/config.py
@@ -1,4 +1,3 @@
-import os
 from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_restful import Api
@@ -6,7 +5,7 @@ from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://kuzzi:K19T804jfy1prp9baIZGKXWc0ZzZi7uV@dpg-ckeefd5tj22c73dl0lqg-a.oregon-postgres.render.com/doghouse_gn5h'
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # app.jsonify_prettify = False  # Adjusted from app.json.compact
